4475ad07cdca23ca488baf4bce30ed9b917083ae
Nagendra Babu R <nagu7654@gmail.com>
2026-08-13 16:55:17 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "mongodb" {
   )
 }
 
-resource "terraform_data" "bootstrap" {
+resource "terraform_data" "mongodb" {
     triggers_replace = [
         aws_instance.mongodb.id
     ]
@@ -22,10 +22,6 @@ resource "terraform_data" "bootstrap" {
         user        = "ec2-user"
         password    = "DevOps321"
         host        = aws_instance.mongodb.private_ip
-
-        bastion_host = "44.203.34.73"
-        bastion_user = "ec2-user"
-        bastion_password = "DevOps321"
     }
 
     provisioner "file" {
@@ -55,7 +51,7 @@ resource "aws_instance" "redis" {
   )
 }
 
-resource "terraform_data" "bootstrap_redis" {
+resource "terraform_data" "redis" {
     triggers_replace = [
         aws_instance.redis.id
     ]
@@ -65,10 +61,6 @@ resource "terraform_data" "bootstrap_redis" {
         user        = "ec2-user"
         password    = "DevOps321"
         host        = aws_instance.redis.private_ip
-
-        bastion_host = "44.203.34.73"
-        bastion_user = "ec2-user"
-        bastion_password = "DevOps321"
     }
 
     provisioner "file" {
@@ -80,6 +72,45 @@ resource "terraform_data" "bootstrap_redis" {
       inline = [
         "chmod +x /tmp/bootstrap.sh",
         "sudo sh /tmp/bootstrap.sh redis"
+      ]
+    }
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-mysql"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "mysql" {
+    triggers_replace = [
+        aws_instance.mysql.id
+    ]
+
+    connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        password    = "DevOps321"
+        host        = aws_instance.mysql.private_ip
+    }
+
+    provisioner "file" {
+      source = "bootstrap.sh" # Local file path
+      destination = "/tmp/bootstrap.sh" # Remote destination path
+    }    
+
+    provisioner "remote-exec" {
+      inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
       ]
     }
 }
