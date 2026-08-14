079d65739d4d026e7c5abf0a56037b231ef7b0b3
Nagendra Babu R <nagu7654@gmail.com>
2026-08-14 17:43:27 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -111,7 +111,46 @@ resource "terraform_data" "mysql" {
     provisioner "remote-exec" {
       inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh mysql"
+        "sudo sh /tmp/bootstrap.sh mysql ${var.environment}"
+      ]
+    }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = local.database_subnet_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-rabbitmq"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+    triggers_replace = [
+        aws_instance.rabbitmq.id
+    ]
+
+    connection {
+        type        = "ssh"
+        user        = "ec2-user"
+        password    = "DevOps321"
+        host        = aws_instance.rabbitmq.private_ip
+    }
+
+    provisioner "file" {
+      source = "bootstrap.sh" # Local file path
+      destination = "/tmp/bootstrap.sh" # Remote destination path
+    }    
+
+    provisioner "remote-exec" {
+      inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq dev"
       ]
     }
 }
