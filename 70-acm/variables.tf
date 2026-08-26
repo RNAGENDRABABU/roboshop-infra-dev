b41c1e66e7ddaece19739ff5380b067609761422
Nagendra Babu R <nagu7654@gmail.com>
2026-08-26 09:39:39 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z0155889GD03W1CNV1LB"
+}
+
+variable "domain_name" {
+  default = "nagendrababu.online"
+}
