d41bad7531bbe54ca56f898a65d3070dad38ddbe
Nagendra Babu R <nagu7654@gmail.com>
2026-08-28 03:54:32 +0530

    terraform

--- 95-cdn/main.tf
+++ 95-cdn/main.tf
@@ -14,7 +14,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
   enabled         = true
   is_ipv6_enabled = false
 
-  aliases = ["${var.project}-${var.environment}-${var.domain_name}"]
+  aliases = ["${var.project}-${var.environment}.${var.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
